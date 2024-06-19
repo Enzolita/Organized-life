f7dc9c606c466225d2c1a8995bfd4c6766c0be42
Elena Hanna <hanna_elena@live.se>
2024-06-19 11:31:50 +0000

    Add a complete function for tasks

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from google.oauth2.service_account import Credentials
 from datetime import datetime
 from tabulate import tabulate
 
-
 """
 To Do List
 """
@@ -33,38 +32,62 @@ ascii_art = r'''
 colored_ascii_art = "\033[92m" + ascii_art + "\033[0m"
 print(colored_ascii_art)
 
-#Data that saves tasks
+
+class Task:
+    def __init__(self, title):
+        self.title = title
+        self.created_at = datetime.now().strftime('%Y-%m-%d')
+        self.is_completed = False
+
+# Data that saves tasks
 dictionary = {}
 todoid = 0
 
 def add_task(task):
     global todoid
     todoid += 1
-    dictionary[todoid] = task
-    print(f"task added {task}, with ID {todoid}")
+    dictionary[todoid] = Task(task)
+    print(f"Task added: {task}, with ID {todoid}")
 
 def remove_task(task_id):
     if task_id in dictionary:
         del dictionary[task_id]
-        print(f"Task deleted with ID: {task_id}")
+        print(f"Task successfully removed with ID: {task_id}")
     else:
         print("Task does not exist.")
 
 def show_tasks():
-    print("Current tasks:")
-    table = []
-    for todoid, task in dictionary.items():
-        print(f"ID: {todoid}, task: {task}")
-        print(tabulate(table, headers=["#", "Task", "Created", "Completed"],
+    if dictionary:
+        print("Current tasks:")
+        table = []
+        for i, (todoid, todo) in enumerate(dictionary.items()):
+            table.append([i + 1, todo.title, todo.created_at, '✅'
+            if todo.is_completed else '❌'])
+        print(tabulate(table, headers=["ID", "Task", "Created", "Completed"],
         tablefmt="grid"))
-    
+    else:
+        print("There are no tasks to show.")
+
+def mark_as_complete():
+    show_tasks()  
+    try:
+        todo_id = int(input("Enter the ID of the task: ")) 
+        if todo_id in dictionary:
+            dictionary[todo_id].is_completed = True
+            print(f"Task with ID {todo_id} marked as completed.")
+        else:
+            print("Invalid todo ID.")  
+    except ValueError:
+        print("Invalid input.") 
+
 while True:
     print("\nWelcome to your To-Do-List:")
     print("What do you want to do?")
     print("1. Add task")
     print("2. Show tasks")
-    print("3. Delete task")
-    print("4. Exit")
+    print("3. Remove task")
+    print("4. Mark task as complete")
+    print("5. Exit")
     choice = input()
     
     if choice == "1":
@@ -78,11 +101,14 @@ while True:
 
     elif choice == "3":
         show_tasks()
-        print("Input the ID of the task you want to delete")
+        print("Input the ID of the task you want to remove")
         task = int(input())
         remove_task(task)
 
     elif choice == "4":
+        mark_as_complete()
+
+    elif choice == "5":
         print("Thank you for using the To-Do-List Application")
         break
         
