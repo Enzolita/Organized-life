c5a8cbe3424e184e55381162059dc6306a62abec
Elena Hanna <hanna_elena@live.se>
2024-06-20 09:10:05 +0000

    Remove whitespaces

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import os
-import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 from tabulate import tabulate
@@ -33,15 +32,18 @@ colored_ascii_art = "\033[92m" + ascii_art + "\033[0m"
 print(colored_ascii_art)
 
 
+
 class Task:
     def __init__(self, title):
         self.title = title
         self.created_at = datetime.now().strftime('%Y-%m-%d')
         self.is_completed = False
 
+
 # Data that saves tasks
 dictionary = {}
 todoid = 0
+
 
 def add_task(task):
     global todoid
@@ -49,12 +51,14 @@ def add_task(task):
     dictionary[todoid] = Task(task)
     print(f"Task added: {task}, with ID {todoid}")
 
+
 def remove_task(task_id):
     if task_id in dictionary:
         del dictionary[task_id]
         print(f"Task successfully removed with ID: {task_id}")
     else:
         print("Task does not exist.")
+
 
 def show_tasks():
     if dictionary:
@@ -68,17 +72,19 @@ def show_tasks():
     else:
         print("There are no tasks to show.")
 
+
 def mark_as_complete():
-    show_tasks()  
+    show_tasks()
     try:
-        todo_id = int(input("Enter the ID of the task: ")) 
+        todo_id = int(input("Enter the ID of the task: "))
         if todo_id in dictionary:
             dictionary[todo_id].is_completed = True
-            print(f"Task with ID {todo_id} marked as completed.")
+            print(f"Task with ID {todo_id} is now marked as completed.")
         else:
-            print("Invalid todo ID.")  
+            print("Invalid task ID.")
     except ValueError:
-        print("Invalid input.") 
+        print("Invalid input.")
+
 
 while True:
     print("\nWelcome to your To-Do-List:")
@@ -89,7 +95,7 @@ while True:
     print("4. Mark task as complete")
     print("5. Exit")
     choice = input()
-    
+
     if choice == "1":
         print("What would you like to add?")
         task = input()
@@ -111,6 +117,6 @@ while True:
     elif choice == "5":
         print("Thank you for using the To-Do-List Application")
         break
-        
+
     else:
         print("Invalid Input")
